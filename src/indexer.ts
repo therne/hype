e6a326b99fdcb678334f073d6723ac01ca9c97ed
therne <retail3210@gmail.com>
2021-10-28 16:50:36 +0900

    feat: Add option to provide log finder builder function

--- src/indexer.ts
+++ src/indexer.ts
@@ -4,9 +4,11 @@ import { defaultOptions, HypeOptions } from './options';
 import { TypedEmitter } from 'tiny-typed-emitter';
 import { BlockDataSource } from './datasource';
 
+type ReturningLogFinderMapperBuilderFn<I> = (block: Block) => Promise<ReturningLogFinderMapper<I>[]>;
+
 export interface Indexer<I, O> {
   id: string;
-  logFinders: ReturningLogFinderMapper<I>[];
+  logFinders: ReturningLogFinderMapper<I>[] | ReturningLogFinderMapperBuilderFn<I>;
   indexer: (input: I, block: Block) => Promise<O[]>;
   postIndexerHook?: (outputs: O[]) => Promise<void>;
 }
@@ -50,14 +52,18 @@ export class Hype extends TypedEmitter<HypeEvents> {
     for await (const block of this.dataSource.blocks()) {
       this.emit('startBlockProcess', block);
       for (const indexerDefinition of this.indexers) {
-        await this.runIndexer(block, indexerDefinition);
+        await this.runSingleIndexer(block, indexerDefinition);
       }
       this.emit('endBlockProcess', block);
     }
   }
 
-  private async runIndexer<I, O>(block: Block, { id, logFinders, indexer: indexerFn, postIndexerHook }: Indexer<I, O>) {
+  async runSingleIndexer<I, O>(
+    block: Block,
+    { id, logFinders: logFinderOrBuilder, indexer, postIndexerHook }: Indexer<I, O>,
+  ) {
     const events = flattenEventsInBlock(block);
+    const logFinders = logFinderOrBuilder instanceof Function ? await logFinderOrBuilder(block) : logFinderOrBuilder;
 
     const inputs: I[] = [];
     for (const event of events) {
@@ -70,7 +76,7 @@ export class Hype extends TypedEmitter<HypeEvents> {
     this.emit('startIndexerRun', id, inputs, block);
     const outputs: O[] = [];
     for (const input of inputs) {
-      const output = await indexerFn(input, block);
+      const output = await indexer(input, block);
       outputs.push(...output);
     }
 
