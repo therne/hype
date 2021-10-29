9a417e1991ce358d67dd98dd59366e46258e846a
therne <retail3210@gmail.com>
2021-10-29 19:39:23 +0900

    test: Rename to Hype

---
+++ test/Hype.test.ts
@@ -0,0 +1,51 @@
+import { Block, Hype } from '../src';
+import fixtureBlock from './fixtures/5036118.json';
+import { createStaticBlockDataSource } from '../src/datasource/BlockDataSource';
+import { createReturningLogFinder } from '@terra-money/log-finder';
+import { findAndParseEvents } from '../src/extensions/log-finder';
+
+interface ClaimAirdropLog {
+  address: string;
+  stage: number;
+  amount: string;
+}
+
+describe('Testing Hype', () => {
+  const block: Block = {
+    height: fixtureBlock.height,
+    timestamp: new Date(fixtureBlock.timestamp),
+    // @ts-ignore
+    transactions: fixtureBlock.transactions,
+  };
+  const hype = new Hype(createStaticBlockDataSource([block]));
+
+  describe('start()', () => {
+    it('should start processing blocks', async () => {
+      let capturedOutputs: ClaimAirdropLog[] = [];
+      hype.subscribe('pylon-buyback', async (block: Block) => {
+        capturedOutputs = findAndParseEvents(block, [
+          createReturningLogFinder(
+            {
+              type: 'from_contract',
+              attributes: [
+                ['contract_address', 'terra1ud39n6c42hmtp2z0qmy8svsk7z3zmdkxzfwcf2'],
+                ['action', 'claim'],
+                ['stage'],
+                ['address'],
+                ['amount'],
+              ],
+            },
+            (_, match): ClaimAirdropLog => ({
+              stage: Number(match[2].value),
+              address: match[3].value,
+              amount: match[4].value,
+            }),
+          ),
+        ]);
+      });
+
+      await hype.start();
+      expect(capturedOutputs).toHaveLength(1);
+    });
+  });
+});
