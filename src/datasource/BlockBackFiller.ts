74fab840cc859289f3be4e8c984e1e81cb11bc09
therne <retail3210@gmail.com>
2021-11-02 03:02:33 +0900

    feat: Retry when error occurred during the backfill (v0.2.5)

--- src/datasource/BlockBackFiller.ts
+++ src/datasource/BlockBackFiller.ts
@@ -1,17 +1,56 @@
 import BlockDataSource from './BlockDataSource';
 import { BlockFetcher } from '../block-fetcher';
 import { Block } from '../block';
+import { log } from '../logger';
+
+export interface BlockBackFillerOptions {
+  interval: number;
+  maxRetry: number;
+  retryIntervalInMs: number;
+}
+
+const defaultBlockPollerOptions: BlockBackFillerOptions = {
+  interval: 0,
+  maxRetry: 10,
+  retryIntervalInMs: 20000,
+};
 
 export default class BlockBackFiller implements BlockDataSource {
-  constructor(public blockFetcher: BlockFetcher, public from: number, public to: number) {}
+  public readonly options: BlockBackFillerOptions;
+
+  constructor(
+    public blockFetcher: BlockFetcher,
+    public from: number,
+    public to: number,
+    options: Partial<BlockBackFillerOptions> = {},
+  ) {
+    this.options = Object.assign(defaultBlockPollerOptions, options);
+  }
 
   async *blocks(): AsyncGenerator<Block> {
     for (let height = this.from; height < this.to; height++) {
-      const block = await this.blockFetcher.fetchBlockAt(height);
-      if (!block) {
-        throw new Error(`block ${height} not available`);
-      }
-      yield block;
+      yield await this.fetchWithRetry(height);
     }
   }
+
+  private async fetchWithRetry(height: number): Promise<Block> {
+    for (let attempt = 0; attempt < this.options.maxRetry; attempt++) {
+      try {
+        const block = await this.blockFetcher.fetchBlockAt(height);
+        if (!block) {
+          throw new Error(`block ${height} not available`);
+        }
+        if (this.options.interval > 0) {
+          await sleep(this.options.interval);
+        }
+        return block;
+      } catch (err) {
+        log('error', 'block-backfiller', `error while fetching a block`, { height, attempt, error: err });
+        await sleep(this.options.retryIntervalInMs);
+      }
+    }
+    throw new Error(`maximum retry attempt ${this.options.maxRetry} exceeded while fetching block #${height}`);
+  }
 }
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
