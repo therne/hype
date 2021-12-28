345bbfe34b3b5bf3e2322259f35018df783df5df
therne <retail3210@gmail.com>
2021-12-28 20:59:42 +0900

    feat: Add options for loadTransactionsOfAccount (FCD backfiller) (v0.2.12)

--- src/datasource/load-transactions-of-account.ts
+++ src/datasource/load-transactions-of-account.ts
@@ -12,7 +12,19 @@ interface FCDResponse {
   txs: Txn[];
 }
 
-export const loadTransactionsOfAccount = (account: string, endpoint = FCD_MAINNET): BlockDataSource => {
+const DEFAULT_SLEEP_MS = 1000;
+
+export interface LoadTransactionsOfAccountOptions {
+  fromBlock?: number;
+  toBlock?: number;
+  sleepMs?: number;
+}
+
+export const loadTransactionsOfAccount = (
+  account: string,
+  endpoint = FCD_MAINNET,
+  options: LoadTransactionsOfAccountOptions = {},
+): BlockDataSource => {
   return {
     async *blocks(): AsyncGenerator<Block> {
       let offset: number | undefined = 0;
@@ -28,21 +40,31 @@ export const loadTransactionsOfAccount = (account: string, endpoint = FCD_MAINNE
         }
 
         // group transactions in the account as a virtual block
-        const blocks = Object.values(groupBy(txs, 'height')).map((transactions) => ({
-          timestamp: new Date(transactions[0].timestamp),
-          height: Number(transactions[0].height),
-          transactions,
-        }));
+        const blocks = Object.values(groupBy(txs, 'height'))
+          .map((transactions) => ({
+            timestamp: new Date(transactions[0].timestamp),
+            height: Number(transactions[0].height),
+            transactions,
+          }))
+          .filter(({ height }) => {
+            if (options.fromBlock) {
+              return height >= options.fromBlock;
+            }
+            if (options.toBlock) {
+              return height < options.toBlock;
+            }
+            return true;
+          });
 
         for (const block of blocks) {
           yield block;
         }
-        await sleep(1000);
+        await sleep(options.sleepMs ?? DEFAULT_SLEEP_MS);
         offset = next;
         page++;
       }
-    }
+    },
   };
-}
+};
 
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
