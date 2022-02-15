bfd20c4527a2408e4e0944986941d5e34c0998c0
therne <retail3210@gmail.com>
2022-02-15 16:28:41 +0900

    docs: Add working example

--- example/simple.ts
+++ example/simple.ts
@@ -1,5 +1,24 @@
-import { BlockPoller, HiveBlockFetcher, Hype } from "../src";
+import { createRealtimeHype } from '../src';
+import { extractEventsInBlock } from '../src/extensions/log-finder';
+import { createCw20TransferLogFinder } from '../src/extensions/cw20';
 
+const anchorTokenTransferFinder = createCw20TransferLogFinder('terra1747mad58h0w4y589y3sk84r5efqdev9q4r02pc');
 
-const blockPoller = new BlockPoller(new HiveBlockFetcher(), )
-const hype = new Hype()
+async function main() {
+  const hype = await createRealtimeHype();
+
+  hype.subscribe('anc-token-transfers', async (block) => {
+    extractEventsInBlock(block, [anchorTokenTransferFinder]).forEach(({ event: { from, to, amount }, txHash }) => {
+      console.log(`Transferred ${amount} ANC: ${from} -> ${to}`);
+      console.log(`  -> https://finder.terra.money/mainnet/tx/${txHash}`);
+    });
+  });
+  await hype.start();
+}
+
+main()
+  .then(() => process.exit())
+  .catch((err) => {
+    console.error(err.stack);
+    process.exit(1);
+  });
