6e04fd30230864e903239e1d8b7e11587ecc4d95
therne <retail3210@gmail.com>
2022-02-18 01:48:09 +0900

    test: Add test for CW20 utilities

---
+++ test/extensions/cw20/log-finder-rules.test.ts
@@ -0,0 +1,18 @@
+import { createCw20SendLogFinder } from '../../../src/extensions/cw20';
+import { HiveBlockFetcher } from '../../../src';
+import { extractEventsInBlock } from '../../../src/extensions/log-finder';
+
+const ANC_TOKEN = 'terra14z56l0fp2lsf86zy3hty2z47ezkhnthtr9yq76';
+
+describe('createCw20SendLogFinder()', () => {
+  const hive = new HiveBlockFetcher();
+  const tokenSendFinder = createCw20SendLogFinder(ANC_TOKEN);
+
+  it('should extract the send log correctly', async () => {
+    const block = await hive.fetchBlockAt(6489827);
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const events = extractEventsInBlock(block!, [tokenSendFinder]);
+    expect(events).toHaveLength(1);
+  });
+});
