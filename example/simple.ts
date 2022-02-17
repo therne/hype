f66d0e564cf80a2a4b6910b6c483c40b23d404b4
therne <retail3210@gmail.com>
2022-02-18 01:47:56 +0900

    chore: Update sample code

--- example/simple.ts
+++ example/simple.ts
@@ -1,6 +1,6 @@
-import { createRealtimeHype } from '../src';
 import { extractEventsInBlock } from '../src/extensions/log-finder';
-import { createCw20TransferLogFinder } from '../src/extensions/cw20';
+import { createCw20SendLogFinder } from '../src/extensions/cw20';
+import { createRealtimeHype } from '../src';
 
 const anchorTokenTransferFinder = createCw20SendLogFinder('terra14z56l0fp2lsf86zy3hty2z47ezkhnthtr9yq76');
 
@@ -9,7 +9,7 @@ async function main() {
 
   hype.subscribe('anc-token-transfers', async (block) => {
     extractEventsInBlock(block, [anchorTokenTransferFinder]).forEach(({ event: { from, to, amount }, txHash }) => {
-      console.log(`Transferred ${amount} ANC: ${from} -> ${to}`);
+      console.log(`Sent ${amount} ANC: ${from} -> ${to}`);
       console.log(`  -> https://finder.terra.money/mainnet/tx/${txHash}`);
     });
   });
